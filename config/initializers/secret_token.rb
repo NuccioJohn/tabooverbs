bd74f47fa72626f5d377ad5bfabf0abe48eed74c
John Nuccio <nuccio.john@gmail.com>
2013-05-28 11:08:12 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tabooverbs::Application.config.secret_token = '990363b20515ff03d678e7676ed36e9b4ed8a6d3e341fc1c4a5b3077b127cf0ac90fe412e198d5fc064c4cf677b087e88142c6398bfab9d94a2e8369f08af1e8'
